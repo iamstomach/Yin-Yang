22b9a5a61e24fe1a5738f620f37e2fb2a19b645e
Bing Liu <henji03@163.com>
2014-03-27 09:25:25 +0800

    Add explain
    change day2-day7 based on day1

--- Predict_vol.R
+++ Predict_vol.R
@@ -44,28 +44,37 @@ forecast(fit,h=7)
 #############################
 # 基本思想
 
-# 用机票订单adv控制不可知突发因素的影响(包括长期的增长趋势)
+# 用机票订单adv控制不可知突发因素的影响(包括长期趋势)
 # 用holiday控制节假日影响
 # 用day(星期几)的虚拟变量控制week的周期变化
 # 用ios/adr的虚拟变量控制发版的影响(在模型中不显著)
 
 # 采用线性回归模型 + AR 模型，在预测值上反映则是：最终预测值 = 线性模型预测值 + 上一期的残差
 
-# 周一的预测值 =  线性模型预测值 + 上周日的预测值偏差
+# 周一的预测值 =  线性模型预测值 + 上一期(上周日)的预测值偏差
 # 周二至周日的预测值 = 周一的预测值 * day虚拟变量的差值
 
-# 举例：
-# log(y1) = x + d1    + e
-# log(y2) = x +    d2 + e
-# 假设X相同，周二的预测值y2 = ex
+# 基于周一预测值预测周二的例子, d1和d2分别为周一和周二虚拟变量的估计值：
+# 周一：log(y1) = x + d1    + e
+# 周二：log(y2) = x +    d2 + e
+# 假设X相同, 已知周一的值y1, 周二的预测值y2 = y1 * exp(d2-d1)
 
+# 留出最后最后三周的数据作为测试集，其他数据作为训练集构建线性模型，最后三周的误差分别为
+# 第一周：5.867458%
+# 第二周：5.720324%
+# 第三周：3.168405%
+
+# 可以改进的地方：预测变量的选择；AR项的阶数，现在只用了上一期的预测残差，相当于只有AR(1), 
+# 可以进行自相关检验，引入高阶的AR项，如前面三期残差的加权平方
 
 
 ## step1: 导入数据----
-setwd("E:\\CP\\predict")
+#setwd("E:\\CP\\predict")
+setwd("N:\\qunar\\Predict")
 order_n <- read.csv("order.csv", header = T, stringsAsFactors = F)
+# 将字符型日期转变成日期格式
 order_n$date <- as.Date(order_n$date)
-# 生成星期几
+# 生成星期几的变量day, 取值1-6分别表示周一到周六，周天取值为0
 order_n$day <- format(order_n$date, "%w")
 head(order_n)
 summary(order_n)
@@ -83,7 +92,7 @@ test_data  <- order_n[ test_num, ]
 
 ## step2: 回归----
 
-# 逐步回归得到模型
+# 逐步回归得到模型，注意订单量进行了对数化
 m0 <- lm(log(hotel_order) ~ log(adv_1) + log(adv_2) + holiday + adr_app + ios_app + day, 
          data = model_data)
 m1 <- step(m0)
@@ -97,6 +106,7 @@ pre_vol <- exp(fitted(m1))
 res <- order_vol - pre_vol
 names(res) <- model_data[, "date"]
 mod_len <- length(res)
+# 生成滞后一期的残差，第一期没有滞后偏差，赋值为缺失值NA
 lagres <- c( NA, res[-mod_len])
 pre_vol2 <- pre_vol + lagres
 
@@ -110,7 +120,9 @@ raw_fit$id <- as.numeric(raw_fit$id)
 raw_fit$vol <- as.numeric(raw_fit$vol)
 summary(raw_fit)
 library(ggplot2)
+# 整段时间区间的对比
 ggplot(raw_fit, aes(x = id, y= vol, color = type)) + geom_line()
+# 第400天以后的对比
 ggplot(raw_fit[raw_fit$id>400, ], aes(x = id, y= vol, color = type)) + geom_line()
 
 
@@ -121,10 +133,10 @@ ggplot(raw_fit[raw_fit$id>400, ], aes(x = id, y= vol, color = type)) + geom_line
 test_data[1:7, ]
 
 
-# 第一步：预测星期一即2014-03-03
-# 星期一的预测变量包括 adv1, adv2, holiday, day, 可以根据系数得出预测值，并利用上一个周一(2014-02-24)的预测偏差进行调整
+# step3.1：预测星期一即2014-03-03的订单量
+# 星期一的预测变量包括 adv1, adv2, holiday, day, 可以根据系数得出预测值，并利用上一期(2014-03-02)的预测偏差进行调整
 
-# 生成公式，选取部分内容黏贴到下面
+# 生成公式，选取部分内容黏贴到下面，避免手写
 para <- coef(m1)
 par_day <- c(para[5:10], 0)
 names(par_day) <- c(1:6,0)
@@ -136,28 +148,31 @@ pre_date <- test_data[id, "date"]
 adv_1 <- test_data[id, "adv_1"]
 adv_2 <- test_data[id, "adv_2"]
 holiday <- test_data[id, "holiday"]
-day <- par_day[1]
+day <- par_day[1] # 周一的虚拟变量取值
 # 模型预测值
 vol1_0 <- exp(3.86788569690671+0.204331344354474*log(adv_1)+0.467433253005349*log(adv_2)+0.243177801879599*holiday+day)
-# 上周同期预测偏差
+# 上期预测偏差
 vol1_adj <- res[as.character(pre_date-1)]
 # 最终预测值
 vol1 <- vol1_0 + vol1_adj
 
-# 第二步：以星期一的预测值作为基础，根据day取值的差距估计周二到周天的订单量,计算预测误差
-pp_vol <- vol1*(1+par_day-day)
-pp_err <- (pp_vol/test_data[1:7, "hotel_order"]-1)*100
+# step3.2：以星期一的预测值作为基础，根据day取值的差距估计周二到周天的订单量,计算预测值pp_vol和预测误差pp_err
+pp_vol <- vol1*exp(par_day-day)
+rr_id <- which(test_data[, "date"] >= as.Date(pre_date) & test_data[, "date"] <= as.Date(pre_date) + 6)
+rr_vol <- test_data[rr_id, "hotel_order"]  # 原始值
+pp_err <- (pp_vol/rr_vol-1)*100
 mean(abs(pp_err))
-# 第一周的平均预测误差为6.33%
+# 第一周的平均预测误差为5.87%
 
 
 ########## 测试集第二周的预测 #############################################
 
 ## 现在已经得到了第一周的真实值，以真实值和模型预测偏差为基础进行分析
 ## 注意区分模型预测值和最终预测值：模型预测值是预测变量和参数的加和，最终预测值是模型预测值和偏差调整值之和
-## 偏差调整值为：上周同期真实指 - 上周同期模型预测值；
+## 偏差调整值为：上一期真实指 - 上一期模型预测值；
 test_data[1:14, ]
 
+# 这里定义了一个函数forcasting()计算某天的模型预测
 forcasting <- function(date, pre_data = test_data){
   pre_date <- as.Date(date)
   id <- which(pre_data[, "date"] == pre_date)
@@ -173,22 +188,24 @@ forcasting <- function(date, pre_data = test_data){
 # 模型预测值
 pre_date <- "2014-03-10"
 vol1_0 <- forcasting(pre_date)
-# 上周同期预测偏差
+# 上一期预测偏差
 last_date <- as.Date(pre_date) - 1
 vol1_adj <- test_data[test_data$date==last_date, "hotel_order"] - forcasting(last_date)
 # 最终预测值
 vol1 <- vol1_0 + vol1_adj
 
 # 第二步：以星期一的预测值作为基础，根据day取值的差距估计周二到周天的订单量,计算预测误差
-pp_vol <- vol1*(1+par_day-day)
+pp_vol <- vol1*exp(par_day-day)
 rr_id <- which(test_data[, "date"] >= as.Date(pre_date) & test_data[, "date"] <= as.Date(pre_date) + 6)
 rr_vol <- test_data[rr_id, "hotel_order"]
 pp_err <- (pp_vol/rr_vol-1)*100
 mean(abs(pp_err))
+# 第二周平均预测误差5.72%
 
 
 
 ########## 测试集第三周的预测 #############################################
+# 这里定义了一个函数pp_test()计算整周的最终预测值
 
 pp_test <- function(date_day1, pre_data=test_data){
   # 模型预测值
@@ -204,7 +221,7 @@ pp_test <- function(date_day1, pre_data=test_data){
   # 第二步：以星期一的预测值作为基础，根据day取值的差距估计周二到周天的订单量,计算预测误差
   id_base <- which(pre_data[, "date"] == pre_date)
   day_base <- par_day[as.character(pre_data[id_base, "day"])]
-  pp_vol <- vol1*(1+par_day-day)
+  pp_vol <- vol1*exp(par_day-day)
   rr_id <- which(pre_data[, "date"] >= as.Date(pre_date) & pre_data[, "date"] <= as.Date(pre_date) + 6)
   rr_vol <- pre_data[rr_id, "hotel_order"]
   pp_err <- (pp_vol/rr_vol-1)*100
@@ -224,6 +241,7 @@ test_data  <- order_n[ test_num, ]
 pp_test("2014-03-03")
 pp_test("2014-02-24")
 pp_test("2014-02-17")
+
 
 
 
