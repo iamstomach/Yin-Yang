960407016b982a0f821d31c64c52a14250384d88
Bing Liu <henji03@163.com>
2014-04-07 17:10:23 +0800

    重新整理文件
    PS: R_wuhan添加ggplot2部分

---
+++ R_wuhan/R_Dzslides.cmd
@@ -0,0 +1,2 @@
+cd
+pandoc -s -S -i -t revealjs --mathjax R_Wuhan.md -o R_W2.html -i -V mainfont="SimSun"  --highlight-style tango
---
+++ R_wuhan/R_js.cmd
@@ -0,0 +1,2 @@
+cd
+pandoc -s -S -i -t revealjs R_Wuhan.md -o R_W2.html -V mainfont="SimSun" -V theme=solarized
